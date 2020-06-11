51e3f20b0d11d0a7b9dd3ca839e864ab17c2b6fa
niceume <toshi@niceume.com>
2020-06-11 11:58:03 +0900

    Some platforms do not support C++11 nullptr. Now nullptr availability is checked in configure.ac, and if it is not available, it is emulated.

--- src/data_sailr_cpp_main.cpp
+++ src/data_sailr_cpp_main.cpp
@@ -44,6 +44,25 @@ typedef unsigned int SXPTYPE;
 #define IF_DEBUG_DECL(x) do{ } while( false )
 #endif
 
+// nullptr support
+
+#ifdef NEEDS_NULLPTR_SUPPORT
+
+const                        // this is a const object...
+class {
+public:
+  template<class T>          // convertible to any type
+    operator T*() const      // of null non-member
+    { return 0; }            // pointer...
+  template<class C, class T> // or any type of null
+    operator T C::*() const  // member pointer...
+    { return 0; }
+private:
+  void operator&() const;    // whose address can't be taken
+} nullptr = {};              // and whose name is nullptr
+
+#endif
+
 /* --------------------------------------------------------- */
 
 // Each element of tuple corresponds to 
