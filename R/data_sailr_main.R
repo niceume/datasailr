f61a77fe6b5f4c2a30dedd03fd7324f05ddcc8c1
niceume <toshi@niceume.com>
2020-06-15 22:10:32 +0900

    Column update did not work properly. Those columns that should be used for column update were wrongly added to the original dataset, and as a result colulmns with the same name were created. This is fixed. Also, when there are columns with the same column name, the leftmost column is used.

--- R/data_sailr_main.R
+++ R/data_sailr_main.R
@@ -19,7 +19,22 @@ sail = function( df , code , fullData = TRUE , rowname = "_rowname_" , stringsAs
 		}
 	}
 
-	result = .data_sailr_cpp_execute( code, df)
+	# If some columns have the same name, left most columns are used
+	ori_colnames = colnames(df)
+	ori_unique_colnames = unique(ori_colnames)
+
+	positions_used_for_each_colname = sapply(ori_unique_colnames, function(unique_name){
+		matched_positions = (ori_colnames %in% unique_name)
+		if(sum(matched_positions) >= 2){
+			cat(sprintf("Note: The original dataset has duplicated variable name( %s ). The leftmost column is used/updated.\n", unique_name))
+		}
+		min(which(matched_positions==TRUE ))
+	}, USE.NAMES = TRUE )
+
+
+	df_wo_duplicated_colnames = df[ positions_used_for_each_colname ]
+
+	result = .data_sailr_cpp_execute( code, df_wo_duplicated_colnames)
 
 	if(stringsAsFactors == TRUE ){
 		result_df = data.frame( lapply(result, function(x) if (is.factor(x)) as.character(x) else {x} ), stringsAsFactors = TRUE )  # Deal strings as factors
@@ -31,8 +46,17 @@ sail = function( df , code , fullData = TRUE , rowname = "_rowname_" , stringsAs
 		df = df[ , !( colnames(df) %in%  rowname )]
 	}
 
+	cols_for_update = colnames(result_df) %in% ori_unique_colnames #logical
+	colnames_for_update = colnames(result_df)[cols_for_update] #character
+	cols_for_addition =  !cols_for_update #logical
 	if(fullData == T){
-		result_df = cbind( df , result_df )
+		# update original columns
+		lapply(colnames_for_update, function(colname_for_update){
+			pos_to_update = positions_used_for_each_colname[colname_for_update]
+			df[pos_to_update] <<- result_df[colname_for_update]
+		})
+		# add new columns
+		result_df = cbind(df , result_df[cols_for_addition])
 	}
 
 	return(result_df)
